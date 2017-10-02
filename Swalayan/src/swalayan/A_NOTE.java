fa550bf80336a7dbe4c4abbf8685ca708c8c892d
linkherz <linkherzg@gmail.com>
2017-10-03 00:05:06 +0700

    note

---
+++ Swalayan/src/swalayan/A_NOTE.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swalayan;
+
+/**
+ *
+ * @author Usui Takumi
+ */
+public class A_NOTE {
+    /*
+    AkunInventory (E) : menyimpan data akun inventory 
+    Autentikasi   (C) : authentikasi dan authorization
+    DataBarang    (E) : menyimpan data barang
+    HalamanAdminInventory   (B)
+    HalamanBarang           (B)
+    HalamanLogin            (B)
+    HalamanSistemInventory  (B)
+    SistemInventory (C)
+    */
+    
+    // (B) : Boundary
+    // (C) : Control
+    // (E) : Entity
+}
