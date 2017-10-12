2777ca1fc317a739238902bc44c69e023f3e711f
wyferion <32459362+wyferion@users.noreply.github.com>
2017-10-12 23:11:17 +0700

    Update SistemInventory.java

--- Swalayan/src/control/SistemInventory.java
+++ Swalayan/src/control/SistemInventory.java
@@ -5,139 +5,52 @@
  */
 package swalayanControl;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import swalayan.HalamanBarang;
+import database.DataBarang;
 
 /**
  *
  * @author USER
  */
 public class SistemInvrntory {
-    ArrayList<DataBarang> list = new ArrayList<DataBarang>();
-        String [] title ={""};
     
-    private Connection connection;
-    private Statement statment;
-    private ResultSet resultSistemInventory;
-    private String kode;
-    private String namabarang;
-    private String kategori;
-    private String harga;
-    private String stock;
-    private String jumlahms;
-    private String jumlahrp;
-
-    public String getKode() {
-        return kode;
-    }
-
-    public void setKode(String kode) {
-        this.kode = kode;
-    }
-
-    public String getNamabarang() {
-        return namabarang;
-    }
-
-    public void setNamabarang(String namabarang) {
-        this.namabarang = namabarang;
-    }
-
-    public String getKategori() {
-        return kategori;
-    }
-
-    public void setKategori(String kategori) {
-        this.kategori = kategori;
-    }
-
-    public String getHarga() {
-        return harga;
-    }
-
-    public void setHarga(String harga) {
-        this.harga = harga;
-    }
-
-    public String getStock() {
-        return stock;
-    }
-
-    public void setStock(String stock) {
-        this.stock = stock;
-    }
-
-    public String getJumlahms() {
-        return jumlahms;
-    }
-
-    public void setJumlahms(String jumlahms) {
-        this.jumlahms = jumlahms;
-    }
-
-    public String getJumlahrp() {
-        return jumlahrp;
-    }
-
-    public void setJumlahrp(String jumlahrp) {
-        this.jumlahrp = jumlahrp;
-    }
+    private DataBarang database;
     
     public SistemInvrntory(){
-        try {
-        connection = DriverManager.getConnection("jdbc:ucanaccess://" + "D:/Kuliah/Semester 5/KPPL/Inventory.accdb;","", "");
-            System.out.println("Berhasil Konek");
-            
-            statment = connection.createStatement();
-            resultSistemInventory = statment.executeQuery("SELECT * FROM Inventory");
-            while (resultSistemInventory.next() == true){
-                list.add(new DataBarang(resultSistemInventory.getString("kodeBarang"),
-                        resultSistemInventory.getString("namaBarang"),
-                        resultSistemInventory.getString("Kategori"),
-                        resultSistemInventory.getString("Harga"),
-                        resultSistemInventory.getString("Stock"),
-                        resultSistemInventory.getString("jumlahMS"),
-                        resultSistemInventory.getString("jumlahRP")));
-            }
-        }   catch (SQLException errMsg){
-            System.out.println(errMsg.getMessage());
-            }
+        
+        database = new DataBarang();
+        
     }
+    
     public void Barang(){
         HalamanBarang guiBrg = new HalamanBarang();
         guiBrg.setVisible(true);
+    
     }
-    public void Buat(){
-        String sql ;
+    
+    public boolean Buat(String kode,String nama,String kategori,String harga,String stock,String ms,String rp){
+        boolean Sql = false;
         
-        sql = "INSERT INTO Inventory (Kode, NamaBRG, Kategori, Harga, Stock, JumlahMS, JumlahRP)" 
-                + "VALUES ('" + kode + "',"
-                + "'" + namabarang + "',"
-                + "'" + kategori + "',"
-                + "'" + harga + "',"
-                + "'" + stock + "',"
-                + "'" + jumlahms + "',"
-                + "'" + jumlahrp + "');";
+        String sql="Insert Into Inventory (Kode, NamaBRG, Kategori, Harga, Stock, JumlahMS, JumlahRP) Values"
+                +"('"+kode+"',"
+                +"'"+nama+"',"
+                + "'"+kategori+"',"
+                + "'"+harga+"',"
+                + "'"+stock+"',"
+                + "'"+ms+"',"
+                +"'"+rp+"')";
         
-        System.out.println(sql);
-        try {
-        int berhasil = statment.executeUpdate(sql);
-        }catch (SQLException errMsg){
-            System.out.println(errMsg.getMessage());
-        }
-        DataBarang barang = new DataBarang();
-        barang.setkodeBarang(kode);
-        barang.setnamaBarang(namabarang);
-        barang.setKategori(kategori);
-        barang.setHarga(harga);
-        barang.setStock(stock);
-        barang.setjumlahMS(jumlahms);
-        barang.setjumlahRP(jumlahrp);
-        this.list.add(barang);
+        Sql = database.InputBarang(sql);
+        
+        return false;     
     }
-}
+    
+    public Object[] LihatLog(int index){
+                
+        Object[] data = new Object[5];
+      
+        data = database.LihatTabel(index);
+                
+        return data;
+    }
+    
+}   
