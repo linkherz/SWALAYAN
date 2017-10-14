316bc2025ae6ebd5481a059221855883f37b63b6
linkherz <linkherzg@gmail.com>
2017-10-14 12:34:53 +0700

    CRUD

--- Swalayan/src/swalayan/SistemInventory.java
+++ Swalayan/src/swalayan/SistemInventory.java
@@ -13,8 +13,57 @@ import javax.swing.JFrame;
  */
 public class SistemInventory {
     
+    private String username;
+    private DataBarang data;
+    
+    public SistemInventory(String u) {
+        this.username = u;
+        data = new DataBarang();
+    }
+    
+    public boolean tambahBarang(String kode, String nama, String kategori, String harga, String stock, String ms, String rp){
+        String sql="INSERT INTO Inventory (Kode, NamaBRG, Kategori, Harga, Stock, MS, RP) VALUES"
+                +"('"+kode+"',"
+                +"'"+nama+"',"
+                + "'"+kategori+"',"
+                + "'"+harga+"',"
+                + "'"+stock+"',"
+                + "'"+ms+"',"
+                +"'"+rp+"')";
+        
+        boolean tambah = data.crdBarang(sql);
+        return tambah;
+    }
+    
+    public Object[][] lihatBarang(){
+        Object[][] dataB = new Object[15][7];
+        dataB = data.lihatBarang();
+        return dataB;
+    }
+    
+    public boolean updateBarang(String kode, String nama, String kategori, String harga, String stock, String ms, String rp){
+        String sql="UPDATE Inventory SET "
+                +"NamaBRG = '"+nama+"',"
+                +"Kategori = '"+kategori+"',"
+                +"Harga = '"+harga+"',"
+                +"Stock = '"+stock+"',"
+                +"MS = '"+ms+"',"
+                +"RP = '"+rp+"' "
+                +"WHERE Kode = '"+kode+"';";
+        
+        boolean update = data.crdBarang(sql);
+        return update;
+    }
+    
+    public boolean hapusBarang(String kode){
+        String sql="DELETE FROM Inventory WHERE Kode = '"+kode+"';"; 
+        
+        boolean hapus = data.crdBarang(sql);
+        return hapus;
+    }
+    
     public void menampilkanHalamanBarang(){
-        HalamanBarang hlmB = new HalamanBarang();
+        HalamanBarang hlmB = new HalamanBarang(username);
         hlmB.setVisible(true);
         hlmB.setExtendedState(JFrame.MAXIMIZED_BOTH);
     }
